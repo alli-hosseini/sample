074536b856ac284178370f33d28592c06077cf17
ali-hosseini <ali_hosseini7785@gmail.com>
2023-01-18 11:34:02 +0330

    add a command handler

--- schedule/application/handlers.py
+++ schedule/application/handlers.py
@@ -2,17 +2,25 @@ from shared.application.interfaces import CommandHandler
 from typing import Optional
 from .commands import CreateSchedule, PublishAppointedSchedules
 from schedule.domain.repositories import ScheduleRepository
+from schedule.domain.value_objects import Vendor
+from schedule.domain.entities import Schedule
+from schedule.domain.value_objects import ScheduleTimeDelta
+from django.utils import timezone
 
 
 class CreateScheduleHandler(CommandHandler):
     def handle(
-        self, command: CreateSchedule, repo: Optional[ScheduleRepository]
+        self, command: CreateSchedule, repo: ScheduleRepository
     ) -> Optional[str]:
-        pass
+        schedule = Schedule(
+            vendor=Vendor(identifier=command.vendor_id, categories=[]),
+            delta=ScheduleTimeDelta(value=timezone.timedelta(hours=2))
+            )
+        repo.persist(schedule)
 
 
 class PublishAppointedHandler(CommandHandler):
     def handle(
-        self, command: PublishAppointedSchedules, repo: Optional[ScheduleRepository]
+        self, command: PublishAppointedSchedules, repo: ScheduleRepository
     ) -> Optional[str]:
         pass
